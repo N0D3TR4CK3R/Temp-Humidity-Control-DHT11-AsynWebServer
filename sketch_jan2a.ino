6af4f00f87bce14dcab3db65ceb0da5ef292d0cd
N0D3TR4CK3R <mateo.cabrera.cedeno@hotmail.com>
2024-01-03 20:23:42 -0500

    Add files via upload

---
+++ sketch_jan2a.ino
@@ -0,0 +1,201 @@
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+// Credenciales de la red WiFi
+const char* ssid = "CELERITY_CABRERA";
+const char* password = "teEze2022*";
+
+// Pin de datos del sensor DHT y tipo de sensor
+#define DHTPIN 27    
+#define DHTTYPE    DHT11     
+DHT dht(DHTPIN, DHTTYPE);
+
+// Inicialización del servidor web en el puerto 80
+AsyncWebServer server(80);
+
+// Función para leer la temperatura del sensor DHT
+String readDHTTemperature() {
+  float t = dht.readTemperature();
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+
+// Función para leer la humedad del sensor DHT
+String readDHTHumidity() {
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(h);
+    return String(h);
+  }
+}
+
+// Contenido de la página web
+const char index_html[] PROGMEM = R"rawliteral(
+  <!DOCTYPE HTML><html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      html {
+      font-family: Arial;
+      display: inline-block;
+      margin: 0px auto;
+      text-align: center;
+      background-color: #fcf0ff;
+      }
+      h2 { 
+        font-size: 3.0rem; 
+        color: rgb(0, 0, 0);
+      }
+      h3 { 
+        font-size: 2.0rem; 
+        color: #312431;
+      }
+      p { 
+        font-size: 3.0rem; 
+      }
+      .units { 
+        font-size: 1.2rem; 
+      }
+      .dht-labels{
+        font-size: 1.5rem;
+        vertical-align:middle;
+        padding-bottom: 15px;
+      }
+    </style>
+  </head>
+  <body>
+    <h2>CONTROL DE TEMPERATURA Y HUMEDAD</h2>
+    <h3>DHT11, Web Asincrona</h3>
+    <p>
+      <svg xmlns="http://www.w3.org/2000/svg" height="40" width="34" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M160 64c-26.5 0-48 21.5-48 48V276.5c0 17.3-7.1 31.9-15.3 42.5C86.2 332.6 80 349.5 80 368c0 44.2 35.8 80 80 80s80-35.8 80-80c0-18.5-6.2-35.4-16.7-48.9c-8.2-10.6-15.3-25.2-15.3-42.5V112c0-26.5-21.5-48-48-48zM48 112C48 50.2 98.1 0 160 0s112 50.1 112 112V276.5c0 .1 .1 .3 .2 .6c.2 .6 .8 1.6 1.7 2.8c18.9 24.4 30.1 55 30.1 88.1c0 79.5-64.5 144-144 144S16 447.5 16 368c0-33.2 11.2-63.8 30.1-88.1c.9-1.2 1.5-2.2 1.7-2.8c.1-.3 .2-.5 .2-.6V112zM208 368c0 26.5-21.5 48-48 48s-48-21.5-48-48c0-20.9 13.4-38.7 32-45.3V144c0-8.8 7.2-16 16-16s16 7.2 16 16V322.7c18.6 6.6 32 24.4 32 45.3z"/></svg>
+      <span class="dht-labels">Temperature</span> 
+      <span id="temperature">%TEMPERATURE%</span>
+      <sup class="units">&deg;C</sup>
+      <svg id="hotAlert" xmlns="http://www.w3.org/2000/svg" height="40" width="34" viewBox="0 0 512 512" style="fill: red;"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+      <svg id="coldAlert" xmlns="http://www.w3.org/2000/svg" height="40" width="34" viewBox="0 0 512 512" style="fill: blue;"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+    </p>
+    <p>
+      <svg xmlns="http://www.w3.org/2000/svg" height="40" width="34" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M96 320c-53 0-96-43-96-96c0-42.5 27.6-78.6 65.9-91.2C64.7 126.1 64 119.1 64 112C64 50.1 114.1 0 176 0c43.1 0 80.5 24.3 99.2 60c14.7-17.1 36.5-28 60.8-28c44.2 0 80 35.8 80 80c0 5.5-.6 10.8-1.6 16c.5 0 1.1 0 1.6 0c53 0 96 43 96 96s-43 96-96 96H96zm-6.8 52c1.3-2.5 3.9-4 6.8-4s5.4 1.5 6.8 4l35.1 64.6c4.1 7.5 6.2 15.8 6.2 24.3v3c0 26.5-21.5 48-48 48s-48-21.5-48-48v-3c0-8.5 2.1-16.9 6.2-24.3L89.2 372zm160 0c1.3-2.5 3.9-4 6.8-4s5.4 1.5 6.8 4l35.1 64.6c4.1 7.5 6.2 15.8 6.2 24.3v3c0 26.5-21.5 48-48 48s-48-21.5-48-48v-3c0-8.5 2.1-16.9 6.2-24.3L249.2 372zm124.9 64.6L409.2 372c1.3-2.5 3.9-4 6.8-4s5.4 1.5 6.8 4l35.1 64.6c4.1 7.5 6.2 15.8 6.2 24.3v3c0 26.5-21.5 48-48 48s-48-21.5-48-48v-3c0-8.5 2.1-16.9 6.2-24.3z"/></svg>
+      <span class="dht-labels">Humidity</span>
+      <span id="humidity">%HUMIDITY%</span>
+      <sup class="units">&percnt;</sup>
+      <svg id="dryAlert" xmlns="http://www.w3.org/2000/svg" height="40" width="34" viewBox="0 0 512 512" style="fill: yellow;"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M375.7 19.7c-1.5-8-6.9-14.7-14.4-17.8s-16.1-2.2-22.8 2.4L256 61.1 173.5 4.2c-6.7-4.6-15.3-5.5-22.8-2.4s-12.9 9.8-14.4 17.8l-18.1 98.5L19.7 136.3c-8 1.5-14.7 6.9-17.8 14.4s-2.2 16.1 2.4 22.8L61.1 256 4.2 338.5c-4.6 6.7-5.5 15.3-2.4 22.8s9.8 13 17.8 14.4l98.5 18.1 18.1 98.5c1.5 8 6.9 14.7 14.4 17.8s16.1 2.2 22.8-2.4L256 450.9l82.5 56.9c6.7 4.6 15.3 5.5 22.8 2.4s12.9-9.8 14.4-17.8l18.1-98.5 98.5-18.1c8-1.5 14.7-6.9 17.8-14.4s2.2-16.1-2.4-22.8L450.9 256l56.9-82.5c4.6-6.7 5.5-15.3 2.4-22.8s-9.8-12.9-17.8-14.4l-98.5-18.1L375.7 19.7zM269.6 110l65.6-45.2 14.4 78.3c1.8 9.8 9.5 17.5 19.3 19.3l78.3 14.4L402 242.4c-5.7 8.2-5.7 19 0 27.2l45.2 65.6-78.3 14.4c-9.8 1.8-17.5 9.5-19.3 19.3l-14.4 78.3L269.6 402c-8.2-5.7-19-5.7-27.2 0l-65.6 45.2-14.4-78.3c-1.8-9.8-9.5-17.5-19.3-19.3L64.8 335.2 110 269.6c5.7-8.2 5.7-19 0-27.2L64.8 176.8l78.3-14.4c9.8-1.8 17.5-9.5 19.3-19.3l14.4-78.3L242.4 110c8.2 5.7 19 5.7 27.2 0zM256 368a112 112 0 1 0 0-224 112 112 0 1 0 0 224zM192 256a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z"/></svg>
+      <svg id="humidAlert" xmlns="http://www.w3.org/2000/svg" height="40" width="34" viewBox="0 0 512 512" style="fill: blue;"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 336c0 79.5 64.5 144 144 144H512c70.7 0 128-57.3 128-128c0-61.9-44-113.6-102.4-125.4c4.1-10.7 6.4-22.4 6.4-34.6c0-53-43-96-96-96c-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32C167.6 32 96 103.6 96 192c0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336z"/></svg>
+    </p>
+  </body>
+
+  <script>
+  // Función que se ejecuta cada 1000 ms para obtener la temperatura
+  setInterval(function ( ) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("temperature").innerHTML = this.responseText;
+        var temperature = parseFloat(this.responseText);
+        // Mostrar el icono de temperatura alta si supera los 22 grados
+        if (temperature > 23) {
+          document.getElementById("hotAlert").style.display = "inline-block";
+        } else {
+          document.getElementById("hotAlert").style.display = "none";
+        }
+        if (temperature < 22) {
+          document.getElementById("coldAlert").style.display = "inline-block";
+        } else {
+          document.getElementById("coldAlert").style.display = "none";
+        }
+      }
+    };
+    xhttp.open("GET", "/temperature", true); // Establece la solicitud GET a la ruta /temperature
+    xhttp.send(); // Envía la solicitud
+  }, 1000 ) ;
+
+  // Función que se ejecuta cada 1000 ms para obtener la humedad
+  setInterval(function ( ) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("humidity").innerHTML = this.responseText;
+        var humidity = parseFloat(this.responseText);
+
+        // Mostrar el icono de humedad alta si es mayor a 60
+        if (humidity > 60) {
+          document.getElementById("humidAlert").style.display = "inline-block";
+        } else {
+          document.getElementById("humidAlert").style.display = "none";
+        }
+
+        // Mostrar el icono de humedad baja si es menor a 30
+        if (humidity < 30) {
+          document.getElementById("dryAlert").style.display = "inline-block";
+        } else {
+          document.getElementById("dryAlert").style.display = "none";
+        }
+      }
+    };
+    xhttp.open("GET", "/humidity", true); // Establece la solicitud GET a la ruta /humidity
+    xhttp.send();// Envía la solicitud
+  }, 1000 ) ;
+  </script>
+  <!--CREDITTOS A
+https://randomnerdtutorials.com/esp32-dht11-dht22-temperature-humidity-sensor-arduino-ide/
+https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf
+-->
+</html>)rawliteral";
+
+// Función que reemplaza los marcadores en la página web con los valores del sensor
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return readDHTTemperature();
+  }
+  else if(var == "HUMIDITY"){
+    return readDHTHumidity();
+  }
+  return String();
+}
+
+void setup(){
+  Serial.begin(115200); // Inicia la comunicación serial para debug
+
+  dht.begin(); // Inicia el sensor DHT
+  
+  // Conexión a la red WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Imprime la IP local del ESP32
+  Serial.println(WiFi.localIP());
+
+  // Rutas para manejar las peticiones HTTP
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTHumidity().c_str());
+  });
+
+  // Inicia el servidor web
+  server.begin();
+}
+ 
+void loop(){
+  
+}
